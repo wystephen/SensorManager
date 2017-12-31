fc00316f40ca1756f8aea19d9b91a9b73e3ea2ae
wang stephen <551619855@qq.com>
2017-12-31 22:31:42 +0800

    ~

--- src/main/java/pers/steve/Sensors/SensorAbstract.java
+++ src/main/java/pers/steve/Sensors/SensorAbstract.java
@@ -1,9 +1,30 @@
 package pers.steve.Sensors;
 
-import javafx.scene.chart.PieChart;
-
 import java.util.concurrent.SynchronousQueue;
 
-public class SensorAbstract<DataElementType> {
+public abstract class SensorAbstract<DataElementType> {
     private final java.util.concurrent.SynchronousQueue<DataElementType> data_queue = new SynchronousQueue<DataElementType>();
+
+    private Thread SensorDataAccepter;
+
+//    protected SensorAbstract(Thread sensorDataAccepter) {
+//        SensorDataAccepter = sensorDataAccepter;
+//    }
+
+
+    /**
+     * Start sensor means set up a new thread for reading
+     * @param State
+     * @return
+     */
+    public abstract  boolean StartSensor(int State);
+
+    /**
+     *
+     * @param State
+     * @return
+     */
+    public abstract  boolean StopSensor(int State);
+
+
 }
