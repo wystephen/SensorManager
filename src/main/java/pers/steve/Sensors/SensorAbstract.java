004d1dfbc8145a5b881f29d523e80cb04834a208
steve <551619855@qq.com>
2018-01-02 16:17:41 +0800

    new uml.

--- src/main/java/pers/steve/Sensors/SensorAbstract.java
+++ src/main/java/pers/steve/Sensors/SensorAbstract.java
@@ -1,9 +1,10 @@
 package pers.steve.Sensors;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.concurrent.SynchronousQueue;
 
 /**
- *
  * @param <DataElementType>
  * @param <DataInterface>
  */
@@ -24,7 +25,62 @@ public abstract class SensorAbstract<DataElementType, DataInterface> {
 
     private String SensorName;
 
+    protected HashSet<SensorDataListener<DataElementType>> listenerHashSet = null;
 
+    protected boolean addDataListener(SensorDataListener<DataElementType> listener) {
+        try {
+            if (listenerHashSet == null) {
+                listenerHashSet = new HashSet<SensorDataListener<DataElementType>>();
+            }
+            listenerHashSet.add(listener);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    protected boolean removeDataListener(SensorDataListener<DataElementType> listener) {
+        try {
+            if (listenerHashSet != null) {
+                listenerHashSet.remove(listener);
+            } else {
+                return false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
+    }
+
+
+    public boolean notifyListeners(DataEvent<DataElementType> event) {
+        try {
+            if (null == listenerHashSet) {
+                return false;
+            }
+            Iterator iter = listenerHashSet.iterator();
+            while (iter.hasNext()) {
+                SensorDataListener<DataElementType> listener =
+                        (SensorDataListener<DataElementType>) iter.next();
+                Runnable task = () -> {
+                    listener.SensorDataEvent(event);
+                };
+                Thread t = new Thread(task);
+                t.start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
 
 
 }
