5b601deaa1b20943075fd299d64aafa416ab5cd7
steve <551619855@qq.com>
2018-01-02 15:21:33 +0800

    new uml.

--- src/main/java/pers/steve/Sensors/UWBDataElement.java
+++ src/main/java/pers/steve/Sensors/UWBDataElement.java
@@ -1,4 +1,13 @@
 package pers.steve.Sensors;
 
 public class UWBDataElement extends SensorsDataElement {
+    /**
+     * Convert Data to String( with '\n' at the end).
+     *
+     * @return
+     */
+    @Override
+    public String convertDatatoString() {
+        return null;
+    }
 }
