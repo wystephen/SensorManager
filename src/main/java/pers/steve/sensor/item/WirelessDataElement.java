0b347b48150d2bacffbac6364903c03111c707c3
steve <551619855@qq.com>
2018-01-03 16:40:13 +0800

    JY901 OK.

--- src/main/java/pers/steve/sensor/item/WirelessDataElement.java
+++ src/main/java/pers/steve/sensor/item/WirelessDataElement.java
@@ -1,11 +1,15 @@
 package pers.steve.sensor.item;
 
+import java.util.HashMap;
+
 /**
  * WirelessDataElement designed for save data of wireless sensor in positioning system.
- * The most important  member variables are (MAC(String)->  Measure(double)) which indicated a map.
+ * The most important  member variables are (MAC(String)->  value(Measure(double),...)) which indicated a map.
  */
 public class WirelessDataElement extends SensorDataElement {
 
+
+    protected HashMap<String, double[]> measurmentsMap = null;
 
 
     /**
@@ -15,6 +19,33 @@ public class WirelessDataElement extends SensorDataElement {
      */
     @Override
     public String convertDatatoString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%15.03f,%15.03f,", time_stamp, system_time_stamp));
+
+        if (measurmentsMap != null) {
+            for (String key : measurmentsMap.keySet()) {
+
+                sb.append(String.format("{%s:%s},", key, convertValuetoString(measurmentsMap.get(key))));
+            }
+
+        } else {
+            sb.append(" Measurements is empty");
+        }
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        sb.append("\n");
+        return sb.toString();
+
     }
+
+
+    protected String convertValuetoString(double[] v) {
+        StringBuilder sb = new StringBuilder();
+        for (double value : v) {
+            sb.append(v);
+            sb.append(",");
+        }
+        return sb.toString();
+
+    }
+
 }
